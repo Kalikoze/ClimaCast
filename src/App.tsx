0d0271ee5c7863b73cc50087989c8f8cbea6566f
Travis Rollins <kalikoze@gmail.com>
2024-05-16 16:38:27 -0700

    Add React Router and linking to new details page.  Also add interfaces for type checking incoming data

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import { getWeather } from './apiCalls';
 import { SearchBar } from './SearchBar';
+import WeatherDetails from './WeatherDetails';
+import { cleanData } from './helpers';
+import { CleanedData } from './interfaces';
 
 const App = () => {
+  const [weatherData, setWeatherData] = useState<CleanedData | null>(null);
+
   const loadWeather = async (city: string) => {
     try {
       const weatherData = await getWeather(city);
-      console.log(weatherData);
+      setWeatherData(cleanData(weatherData));
     } catch (error) {
       console.error(error);
     }
   }
 
   return (
-    <main className="h-screen bg-gradient-to-r from-gray-900 via-blue-900 to-gray-900 text-white">
-      <header className="h-2/5 flex flex-col items-center justify-center">
-        <h1 className="text-5xl font-bold p-8">
-          ClimaCast
-        </h1>
-        <div className="w-full max-w-md p-8 bg-gray-800 bg-opacity-90 rounded-lg shadow-lg">
-          <SearchBar loadWeather={loadWeather} />
-        </div>
-      </header>
-    </main>
+    <Router>
+      <main className="h-screen bg-gradient-to-r from-gray-900 via-blue-900 to-gray-900 text-white">
+        <header className="h-2/5 flex flex-col items-center justify-center">
+          <h1 className="text-5xl font-bold p-8">
+            ClimaCast
+          </h1>
+          <div className="w-full max-w-md p-8 bg-gray-800 bg-opacity-90 rounded-lg shadow-lg">
+            <Routes>
+              <Route path="/" element={<SearchBar loadWeather={loadWeather} />} />
+              <Route path="/weather/:city" element={<WeatherDetails weatherData={weatherData} />} />
+            </Routes>
+          </div>
+        </header>
+      </main>
+    </Router>
   )
 }
 
